f8b5eb1d50aa21f63c16d40adc7efc35c5e61d8e
william <williamjean.k.o@gmail.com>
2021-04-18 14:58:52 +0200

    laved login

---
+++ TAO_Project/TAO_Project/Controllers/CalculationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TAO_Project.Models;
+
+namespace TAO_Project.Controllers
+{
+    public class CalculationController : Controller
+    {
+        // GET: Calculation
+        public ActionResult Index()
+        {
+            Discounts discounts = new Discounts();
+            discounts.FillList();
+            ViewBag.Discounts = discounts;
+            return View();
+        }
+    }
+}
